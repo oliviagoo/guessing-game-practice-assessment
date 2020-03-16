be4cabd4f19b06be1944be859a8925c6d2404fee
Olivia Goodman <oliviagoodman@Olivias-MacBook-Air.local>
2020-03-17 10:01:23 +1300

    creating and testing version 1
    in this version I get the basics of the program working - a random number and a guess

--- guessing game.py
+++ guessing game.py
@@ -1,3 +1,21 @@
 #olivia g 17/3/20
 #version 1
 #in this version I will check if input is the same as a random number
+
+#importing the random module
+import random
+
+#getting and storing random number
+num = random.randint(1, 100)
+
+#printing the number for testing
+print(num)
+
+#input
+guess = int(input("Guess what number I am thinking of: "))
+
+#output
+if guess == num:
+    print("Correct")
+else:
+    print("Incorrect")
