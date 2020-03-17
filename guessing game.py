079ee519327b21878937b9b335857b7551f89750
Olivia Goodman <oliviagoodman@Olivias-Air.hub>
2020-03-17 18:48:37 +1300

    creating and testing version 4
    in this version the number of guesses that the user gets is limited to 10, and if they reach ten then they lose

--- guessing game.py
+++ guessing game.py
@@ -1,9 +1,13 @@
 #olivia g 17/3/20
-#version 3
-#in this version I will give the user helpful hints about their guess
+#version 4
+#in this version I will count and limit the number of guesses
 
 #importing the random module
 import random
+
+#setting a guess limit of 10, and setting up guess counter
+GUESSES = 10
+guess_num = GUESSES
 
 #getting and storing random number
 num = random.randint(1, 100)
@@ -11,7 +15,7 @@ num = random.randint(1, 100)
 #printing the number for testing
 print(num)
 
-#trying until the user gets it right
+#trying until the user gets it right  or runs out of guesses
 while True:
     guess = int(input("Guess what number I am thinking of: "))
     if guess == num:
@@ -19,10 +23,13 @@ while True:
         break
     else:
         print("Incorrect")
-        if guess < num:
-            print("The number is higher!")
-        #if the guess isn't the number and isn't lower, it must be higher
+        guess_num -= 1
+        if guess_num > 0:
+            if guess < num:
+                print("The number is higher!")
+            #if the guess isn't the number and isn't lower, it must be higher
+            else:
+                print("The number is lower!")
         else:
-            print("The number is lower!")
-
-
+            print("You ran out of guesses!")
+            break
